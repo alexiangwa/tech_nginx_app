156fdd10275912662c401e5626c16833948c46c1
alexiangwa <99841440+alexiangwa@users.noreply.github.com>
2023-08-02 00:57:47 -0500

    Update ec2.tf
    updated sg

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "tech_nginx_web" {
     key_name = var.keypair
     user_data = file("${path.module}/nginx.sh")
     subnet_id = aws_subnet.tech_nginx_pub_sn1.id
-    vpc_security_group_ids = [aws_security_group.tech_nginx_vpc_ssh.id]
+    vpc_security_group_ids =[aws_security_group.tech_nginx_app_vpc_web.id,aws_security_group.tech_nginx_app_vpc_web.id]
     associate_public_ip_address =  true	
 
     tags = {
